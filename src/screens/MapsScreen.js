0a100e97ce94e8a1278d9d4d6f95620f9bcec7dd
nur hasan <nhasan716@gmail.com>
2019-11-15 02:37:18 +0700

    Showing Map

--- src/screens/MapsScreen.js
+++ src/screens/MapsScreen.js
@@ -6,14 +6,30 @@ import {
   Alert,
   TouchableOpacity
 } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function MapsScreen(props) {
     return(
-        <View>
-            <Text>
-                MAPS SCREEN
-            </Text>
-        </View>
+       
+        <MapView
+        style={{ flex: 1, width: window.width }} //window pake Dimensions
+        region={{
+           latitude: 37.78825,
+           longitude: -122.4324,
+           latitudeDelta: 0.0922,
+           longitudeDelta: 0.0421 
+        }} >
+     <MapView.Marker
+        coordinate={{
+           latitude: 37.78825,
+           longitude: -122.4324,
+        }}
+        title="Lokasi"
+        description="Hello" />
+     </MapView>
+
+     
+        
     )
 
 }
